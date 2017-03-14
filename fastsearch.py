2276cff437c8424de9f9dcf415c8488f99283e2d
left <mr@dsain.de>
2017-03-14 22:34:01 +0100

    Replaced the writing functionality with multiple tweet compatibility

--- fastsearch.py
+++ fastsearch.py
@@ -9,9 +9,14 @@ consumer_secret = ignore.TWITTER_CONSUMER_SECRET
 access_token = ignore.TWITTER_ACCESS_TOKEN
 access_secret = ignore.TWITTER_ACCESS_SECRET
 
-d = datetime.datetime.now()
-d = str(d.replace(second=0, microsecond=0))
+#in order to only get minutious file names we set the second and microsecond
+#in the datetime to 0
+d = str(datetime.datetime.now().replace(second=0, microsecond=0))
 
+
+#in order to receive a bearer token for OAuth2 authentication
+#this method can be called - once a token is generated the method will
+#always return this one until it is invalidated/invalid
 def get_bearer_token(consumer_key, consumer_secret):
     # get bearer token for application only requests
     bearer_token_credentials = base64.urlsafe_b64encode('{}:{}'.format(consumer_key, consumer_secret).encode('ascii')).decode('ascii')
@@ -40,25 +45,29 @@ def get_bearer_token(consumer_key, consumer_secret):
 # link the list elements with a + (for example %23kekstortenauflauf+%23wurstbrotkonfetti)
 # function for replacing @/# with the respective %-symbols will follow in order to
 # make the application web compatible
-query ='%23wurstbrotkonfetti'
+query ='%23science'
+
+def replace_non_ascii(query):
+    print(query)
+
 
 def super_fast_hashtag_query(query):
     bearer_token = ignore.BEARER_TOKEN
     url = 'https://api.twitter.com/1.1/search/tweets.json?q={}'.format(query)
     headers = {'Authorization': 'Bearer '+ bearer_token + '', 'User-Agent': 'science analysis client'}
     r = requests.get(url, headers=headers)
-
+    #print(r.text)
+    counter = 0
     tweet_dictionary = json.loads(r.text)
+    testliste = [tweet_dictionary['statuses']]
     with open('datamining_{}.txt'.format(d), 'w') as f:
-        f.write(tweet_dictionary['statuses'][0]['created_at']+"\n")
-        f.write(tweet_dictionary['statuses'][0]['user']['id_str']+"\n")
-        f.write(tweet_dictionary['statuses'][0]['user']['name']+"\n")
-        f.write(tweet_dictionary['statuses'][0]['text']+"\n")
-
-        #print(tweet_dictionary['statuses'][0]['user']['id_str'])
-        #print(tweet_dictionary['statuses'][0]['user']['name'])
-        #print(tweet_dictionary['statuses'][0]['text'])
+        for items in testliste:
+            for item in items:
+                f.write(item['created_at']+"\n")
+                f.write(item['id_str']+"\n")
+                f.write(str(item['text'].encode('UTF-8'))+"\n\n")
     f.close()
+    
 
 
 
