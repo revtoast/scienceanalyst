5dc3666a4f6df9d72f51a2b0f82fa44d61d12f56
left <mr@dsain.de>
2017-03-16 18:52:21 +0100

    Fehlersuche fuer Joern :/

--- fastsearch.py
+++ fastsearch.py
@@ -3,6 +3,7 @@ import ignore
 import json
 import requests
 import datetime
+import time
 
 consumer_key = ignore.TWITTER_CONSUMER_KEY
 consumer_secret = ignore.TWITTER_CONSUMER_SECRET
@@ -12,6 +13,11 @@ access_secret = ignore.TWITTER_ACCESS_SECRET
 #in order to only get minutious file names we set the second and microsecond
 #in the datetime to 0
 d = str(datetime.datetime.now().replace(second=0, microsecond=0))
+
+
+#this specifies the date 7 days back in order to retrieve tweets that were posted within the last 7 days
+today = datetime.date.today() + datetime.timedelta(days=1)
+lastweek = today - datetime.timedelta(days=3)
 
 
 #in order to receive a bearer token for OAuth2 authentication
@@ -45,33 +51,95 @@ def get_bearer_token(consumer_key, consumer_secret):
 # link the list elements with a + (for example %23kekstortenauflauf+%23wurstbrotkonfetti)
 # function for replacing @/# with the respective %-symbols will follow in order to
 # make the application web compatible
-query ='%23science'
-
+query ='%23ICURehab'
+zaehler = 0
+#still needs to be implemented
 def replace_non_ascii(query):
     print(query)
 
 
-def super_fast_hashtag_query(query):
+def super_fast_hashtag_query(query, lastid = 0):
     bearer_token = ignore.BEARER_TOKEN
-    url = 'https://api.twitter.com/1.1/search/tweets.json?q={}'.format(query)
+
+    if lastid == 0:
+        url = 'https://api.twitter.com/1.1/search/tweets.json?q={}&result_type=recent&since={}&until={}&count=10000'.format(query, lastweek, today)
+        schreibtyp = 'w'
+
+
+    else:
+        url = 'https://api.twitter.com/1.1/search/tweets.json?q={}&result_type=recent&max_id={}&count=10000'.format(query, lastid)
+        schreibtyp = 'a'
+
+
     headers = {'Authorization': 'Bearer '+ bearer_token + '', 'User-Agent': 'science analysis client'}
     r = requests.get(url, headers=headers)
     #print(r.text)
-    counter = 0
+
     tweet_dictionary = json.loads(r.text)
     testliste = [tweet_dictionary['statuses']]
-    with open('datamining_{}.txt'.format(d), 'w') as f:
-        for items in testliste:
-            for item in items:
-                f.write(item['created_at']+"\n")
-                f.write(item['id_str']+"\n")
-                f.write(str(item['text'].encode('UTF-8'))+"\n\n")
-    f.close()
-    
+    data_dictionary = {}
+    #letzteid = str(tweet_dictionary['search_metadata']['max_id'])
+    #print (letzteid)
+
+    # das funktioniert nicht und ich sehe den fehler nicht...
+    # theoretisch soll der die letzte ID nehmen und checken ob die schon im dict
+    # ist und wenn ja einfach abbrechen - aber irgendwie läuft der trotz gleich
+    # bleibender ID einfach weiter... 
+    for items in testliste:
+        for item in items:
+            #print (type(item['id_str']))
+
+            if item['id_str'] in data_dictionary.keys():
+                print("done")
+                return data_dictionary
+
+            else:
+                print(item['id_str'])
+                ts = time.strftime('%Y-%m-%d %H:%M', time.strptime(item['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+                #data_dictionary[item["id_str"]] = [ts, item['user']['id_str'], item['user']['screen_name'], item['retweet_count'], str(item['text'].encode('UTF-8'))[2:-1]]
+                data_dictionary[item["id_str"]] = [ts, item['user']['id_str'], item['user']['screen_name'], item['retweet_count'], str(item['text'].encode('UTF-8'))[2:-1]]
+                letzteid = item['id_str']
+        #if not letzteid in data_dictionary.keys():
+    super_fast_hashtag_query(query, letzteid)
+
+
+#this is the earliest ID of the query -> the starting point for the next query
+        #
 
 
 
-super_fast_hashtag_query(query)
+
+'''
+    with open('files/datamining_{}.txt'.format(d), '{}'.format(schreibtyp)) as f:
+            for items in testliste:
+                for item in items:
+                        letzteid = items[-1]['id_str']
+                    #this
+                    #if not 'RT' in item['text']:
+                        print ("itemid: {}".format(item['id_str']))
+
+                        global zaehler
+                        f.write(str(zaehler)+"\n")
+                        print(zaehler)
+                        ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(item['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+                        f.write("{}".format(ts)+"\n")
+                        f.write(item['id_str']+"\n")
+                        f.write(item['user']['id_str']+"\n")
+                        f.write("@"+item['user']['screen_name']+"\n")
+                        f.write("retweetcount: "+str(item['retweet_count'])+"\n")
+                        #f.write("favcount: "+str(item['favorite_count'])+"\n")
+                        f.write(str(item['text'].encode('UTF-8'))[2:-1]+"\n\n")
+                        zaehler += 1
+                        if str(lastid) == str(letzteid):
+                            return
+
+'''
+
+
+
+dic = super_fast_hashtag_query(query)
+for key, value in dic.items():
+    print (key, value)
 
 # **************      for the DF we need      ************** #
 # - time or tweetID as index
