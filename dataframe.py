92d3d4333dcb5b936862f05b456755a570ce9668
left <mr@dsain.de>
2017-03-19 15:48:46 +0100

    Cleanup + plotting implementation in dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,8 +1,18 @@
-import sqlite3
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 import pandas as pd
+import sqlite3
+
+
 # Create your connection.
 cnx = sqlite3.connect('DB/parser.db')
 
-df = pd.read_sql_query("SELECT * FROM twitter_icurehab", cnx)
+df = pd.read_sql_query("SELECT * FROM twitter_stock", cnx)
+dataseries = df['date'].value_counts()
+dataseries.sort_index(axis='index')
 
-print(df.head())
+dataseries.index = pd.to_datetime(dataseries.index)
+dataseries.plot(x='index', y='values')
+plt.show()
+#plt.savefig('test.png')
